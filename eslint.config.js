58d819bc307fca1421a09647f0dc59205c152327
EemanAsad <eemanasad89@gmail.com>
2025-08-25 01:29:14 +0500

    Fixed lint issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,13 @@
-import { FlatCompat } from "@eslint/eslintrc";
-
-const compat = new FlatCompat({
-  baseDirectory: process.cwd(),
-  recommended: true,
-});
-
 export default [
-  ...compat.extends("eslint:recommended"),
   {
     files: ["**/*.js"],
-    rules: {
-      "no-console": "off",
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
     },
-  },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
 ];
